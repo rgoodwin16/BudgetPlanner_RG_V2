23998d00c8417c55cd57f65c059884a98b4dc23b
Ray Goodwin <rgoodwin16@outlook.com>
2015-09-10 22:43:24 -0400

    9/10/15
    Sleeps? I can Has?

--- BudgetPlanner_RG_V2/BudgetPlanner_RG_V2/Models/Code First/BudgetItem.cs
+++ BudgetPlanner_RG_V2/BudgetPlanner_RG_V2/Models/Code First/BudgetItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,9 @@ namespace BudgetPlanner_RG_V2.Models
 
         public int Frequency { get; set; }
 
+        [JsonIgnore]
         public virtual HouseHold HouseHold { get; set; }
+        [JsonIgnore]
         public virtual Category Category { get; set; }
 
     }
